1238a8aa3b2c54ada60e2224330589fbc6a17037
youngmakcoc <yungmakcoc@gmail.com>
2022-01-10 21:06:42 +0500

    Бот отвечает на сообщения

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-import json
+import json, telebot, requests
 
-import requests
+
+bot = telebot.TeleBot("5092500763:AAFP_xoeiMwFMECkZVInlJiyWB-qLYU-PcA") #токен бота
 
 def getAnek():
-    response=requests.get("http://rzhunemogu.ru/RandJSON.aspx?CType=1",headers={"Content-Type":"application/json; charset=windows-1251"}).content.decode("cp1251")
+    response=requests.get("http://rzhunemogu.ru/RandJSON.aspx?CType=1",headers={"Content-Type":"application/json; charset=windows-1251"}).content.decode("cp1251") # запрос анекдота
     anek=json.loads(response, strict=False)["content"]
     print(anek)
-    try:
-        message=requests.post("https://api.telegram.org/bot5092500763:AAFP_xoeiMwFMECkZVInlJiyWB-qLYU-PcA/sendMessage",data={"chat_id":-783348924,"text": anek})
-    except BaseException:
-        getAnek()
+    return anek
 
-getAnek()
+@bot.message_handler(content_types=["text"])
+def getMessage(message):
+    if message.text.lower()=="дай анекдот":
+        try:
+            bot.send_message(chat_id=message.from_user.id, text=getAnek())
+        except:
+            getAnek()
+    else:
+        bot.send_message(chat_id=message.from_user.id, text="Напиши 'дай анекдот'")
+
+
+
+bot.polling(none_stop=True, interval=0)
+
